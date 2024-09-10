58e94acdd85404fb2236a45e7d21d541e17914f5
King_of_kings <kurov666777@gmail.com>
2024-09-11 01:01:33 +0300

    task8_y2

--- lab1/client_script.sh
+++ lab1/client_script.sh
@@ -2,5 +2,5 @@ sudo mv ~/resolvconf.conf /etc
 sudo resolvconf -u
 mkdir -p ~/local_certs
 export CAROOT="~/local_certs"
-mv ~/rootCA.pem ~/local_certs
+mv ~/local.pem ~/local_certs
 mkcert --install
--- lab1/setup_client.sh
+++ lab1/setup_client.sh
@@ -1,2 +1,2 @@
-scp ~/.local/share/mkcert/rootCA.pem resolvconf.conf client_script.sh client@10.0.2.15:~/
+scp local.pem resolvconf.conf client_script.sh client@10.0.2.15:~/
 ssh client@10.0.2.15 -t "./client_script.sh"
