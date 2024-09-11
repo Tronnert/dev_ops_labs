6dc0aa53694e2b50fe2ecec3ab3d71818d8f4615
King_of_kings <kurov666777@gmail.com>
2024-09-11 13:48:26 +0300

    task9_bl

--- lab1/memes/main.py
+++ lab1/memes/main.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
+    # app.se
+    # return url_for("dir1", filename="goldfish.jpg")
     return "hell\n"
-    # return "hell\n"
