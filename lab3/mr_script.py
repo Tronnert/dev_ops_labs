7375e59c3b6f2389246f791453e8296b8c42868b
staffeev <st.affeev@yandex.ru>
2024-10-20 03:30:07 +0300

    test 2

--- lab3/mr_script.py
+++ lab3/mr_script.py
@@ -10,24 +10,24 @@ class DAUmetric(MRJob):
 
     def reducer(self, key, _):
         yield key[0], key[1]
-    
+
     def reducer2(self, key, values):
         yield key, sum(1 for _ in values)
-    
+
     def mapper2(self, date, users):
         yield date[:-3], (date, users)
-    
+
     def reducer3(self, key, values):
         list_values = list(values)
         sum_users = sum(i[1] for i in list_values)
         yield None, (key, sum_users, list_values)
-    
+
     def reducer4(self, key, values):
         dates = min(values, key=lambda x: x[1])[2]
         dates.sort(key=lambda x: int(x[0].split("-")[2]))
         for date, dau in dates:
             yield date, dau
-    
+
     def steps(self):
         return [
             MRStep(mapper=self.mapper, reducer=self.reducer),
@@ -39,4 +39,3 @@ class DAUmetric(MRJob):
 
 if __name__ == "__main__":
     DAUmetric.run()
-    
